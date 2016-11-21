e8437063ead6234e57b35ae0ac3fc701d23fb72d
neyb <noo.blaster@gmail.com>
2016-11-21 08:48:28 +0100

    add simplematcher

---
+++ src/main/kotlin/io/github/neyb/shoulk/SimpleMatcher.kt
@@ -0,0 +1,14 @@
+package io.github.neyb.shoulk
+
+infix fun <T> ((T) -> Boolean).describeFailure(failureDescriptionBuilder: (T) -> String) =
+        SimpleMatcher(this, failureDescriptionBuilder)
+
+class SimpleMatcher<T>(
+        private val criteria: (T) -> Boolean,
+        private val failureDescriptionBuilder: (T) -> String)
+    : Matcher<T> {
+
+    override fun match(actual: T) = criteria(actual)
+
+    override fun getDismatchDescriptionFor(actual: T) = failureDescriptionBuilder(actual)
+}
